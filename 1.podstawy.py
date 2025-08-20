95fac18bbb040ddecf0ee41331636859bd3903e6
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2025-08-20 12:40:22 +0200

    add file with basics

--- 1.podstawy.py
+++ 1.podstawy.py
@@ -1,7 +1,20 @@
+
 # Python to język interpretowany (nie trzeba kompilować), tylko interpretowany – kod jest wykonywany linijka po linijce.
 
 # Wpisując "python" w terminalu, wchodzisz w tryb interaktywny (REPL)
 # Aby zakończyć: wpisz exit() albo Ctrl + Z (Windows) / Ctrl + D (Linux/macOS)
+#Mogę uruchomić srodowisko do Pythona przez przeglądarke-Jupyter Notebook 
+
+#Jupyter Notebook(Anaconda) który można również zainstalować i uzywać na komputerze- ale muszę mieć zaintalowanego już Pythona
+#Kod można pisać w dowolnym edytorze, choćby w notatniku, i poprzez uruchominie pliku w lokalnym wierszu poleceń(cmd), wywoływac plik
+#Edytorów do pracy z pythonem jest na prawde dużo np.Spyder
+
+'''gddsfdsf''' #komentarz blokowy :)
+
+#Funkcja print() - console.log()
+#Może wyświetlać jedną- wiele wartości, przedzielonych przecinkami
+#Wyświetlać można teksty, zmienne, liczby, mogę umieścić znaki specjalne
+
 
 # --- WYPISYWANIE NA EKRAN ---
 
